980b7baa59f1dd32cf64d2726f66a1eb86fe4b82
J165_이창엽 <dlckdduq97@gmail.com>
2021-11-07 20:13:30 +0900

    2021.11.07

--- Solutions/하루 한문제 코테준비/욕심쟁이판다_백준_1937.py
+++ Solutions/하루 한문제 코테준비/욕심쟁이판다_백준_1937.py
@@ -1,38 +1,35 @@
-import copy
 import sys
-sys.setrecursionlimit(10000)
-def dfs(sx,sy,count,graph,visited):
-    global result
-    result = max(result,count)
-    visited[sx][sy] = True
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+def dfs(sx,sy):
+    if(visited[sx][sy]):
+        return visited[sx][sy]
+
+    visited[sx][sy] = 1
     currentValue = graph[sx][sy]
-    # if currentValue:
-    #
-    # else:
     for i in range(4):
         nx,ny = sx+dx[i],sy+dy[i]
         if(0<=nx<n and 0<=ny<n):
             nextValue = graph[nx][ny]
-            if(currentValue<nextValue and not visited[nx][ny]):
-                visited[nx][ny] = True
-                dfs(nx,ny,count+1,graph,visited)
-                # print(count,sx,sy,nx,ny)
-                visited[nx][ny] = False
+            if(currentValue<nextValue):
+                visited[sx][sy] = max(visited[sx][sy],dfs(nx,ny)+1)
 
-    # return count
+    return visited[sx][sy]
 
 
 dx,dy = [0,0,1,-1],[1,-1,0,0]
 n = int(input())
 result = 0
 graph = []
+visited = [[0 for i in range(n)] for j in range(n)]
+
 for i in range(n):
     graph.append(list(map(int,input().split(" "))))
 
+
 for i in range(n):
     for j in range(n):
-        cpyGraph = copy.deepcopy(graph)
-        visited = [[False for i in range(n)] for j in range(n)]
-        dfs(i,j,1,cpyGraph,visited)
+        result = max(result,dfs(i,j))
 
 print(result)
