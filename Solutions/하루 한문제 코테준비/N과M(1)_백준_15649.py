2922c2d8049d4674257b30880c6800a206bd57e0
J165_이창엽 <dlckdduq97@gmail.com>
2022-01-27 02:10:48 +0900

    2022.1.27 백트래킹으로 수정

--- Solutions/하루 한문제 코테준비/N과M(1)_백준_15649.py
+++ Solutions/하루 한문제 코테준비/N과M(1)_백준_15649.py
@@ -1,8 +1,13 @@
-from itertools import permutations
+def dfs():
+    if(len(arr)==m):
+        print(' '.join(list(map(str,arr))))
+        return
+    else:
+        for i in range(1,n+1):
+            if(i not in arr):
+                arr.append(i)
+                dfs()
+                arr.pop()
 n,m = map(int,input().split(" "))
-
-arr = [i for i in range(1,n+1)]
-for i in permutations(arr,m):
-    for j in i:
-        print(j,end=' ')
-    print()
+arr = []
+dfs()
