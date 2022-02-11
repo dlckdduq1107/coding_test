68fecacd7cad1cb82b94f7efea303331fc6ff0cf
J165_이창엽 <dlckdduq97@gmail.com>
2022-02-11 12:00:05 +0900

    2022.2.11
    재귀를 combinatrions로 바꿀 수 있다.

--- Solutions/하루 한문제 코테준비/냅색문제_백준_1450.py
+++ Solutions/하루 한문제 코테준비/냅색문제_백준_1450.py
@@ -1,4 +1,5 @@
 import bisect
+from itertools import combinations
 def dfs(origin,sum_arr,count,value):
     if(count>=len(origin)):
         sum_arr.append(value)
@@ -11,12 +12,19 @@ weight = list(map(int,input().split()))
 
 fw,sw = weight[:len(weight)//2],weight[len(weight)//2:]
 first,second = [],[]
-dfs(fw,first,0,0)
-dfs(sw,second,0,0)
+# dfs(fw,first,0,0)
+# dfs(sw,second,0,0)
 
-# print(first,second)
+for i in range(len(fw)+1):
+    for each in combinations(fw,i):
+        first.append(sum(each))
+for i in range(len(sw)+1):
+    for each in combinations(sw,i):
+        second.append(sum(each))
+
 second.sort()
 result = 0
+# print(first,second)
 for i in first:
     # print(bisect.bisect_right(second,c-i),i)
     if(c-i<0):
