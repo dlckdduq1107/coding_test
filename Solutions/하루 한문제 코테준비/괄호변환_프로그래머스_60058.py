cb952318f729f706643e88de53ebf58d744f8fe2
J165_이창엽 <dlckdduq97@gmail.com>
2022-03-02 12:12:10 +0900

    2022.3.2

--- Solutions/하루 한문제 코테준비/괄호변환_프로그래머스_60058.py
+++ Solutions/하루 한문제 코테준비/괄호변환_프로그래머스_60058.py
@@ -1,3 +1,14 @@
+def check(string):
+    temp = {')':'('}
+    stack = []
+    for i in string:
+        if(i not in temp):
+            stack.append(i)
+        else:
+            if(not stack or temp[i] != stack.pop()):
+                return False
+    return len(stack)==0
+
 def solution(p):
     answer = ''
     return answer
