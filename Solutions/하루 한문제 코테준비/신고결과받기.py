2677f44aadf65e1b000baf057f7367f761c4c5c9
이창엽(Fansor) <dlckdduq97@gmail.com>
2022-05-27 01:35:10 +0900

    2022.5.27

---
+++ Solutions/하루 한문제 코테준비/신고결과받기.py
@@ -0,0 +1,21 @@
+def solution(id_list, report, k):
+    answer = []
+    limit = {}
+    gobal = {}
+    for i in id_list:
+        limit[i] = 0
+        gobal[i] = []
+    for i in report:
+        attack, target = i.split(" ")
+        if(target not in gobal[attack]):
+            gobal[attack].append(target)
+            limit[target] += 1
+    # print(gobal)
+    # print(limit)
+    for i in id_list:
+        temp = 0
+        for j in gobal[i]:
+            if(limit[j]>=k):
+                temp += 1
+        answer.append(temp)
+    return answer
