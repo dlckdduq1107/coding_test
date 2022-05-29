25c1e036ea3dd0ca3e730b9453cfd084be02fecf
이창엽(Fansor) <dlckdduq97@gmail.com>
2022-05-30 08:09:51 +0900

    2022.5.30

---
+++ Solutions/하루 한문제 코테준비/블랙잭_백준.py
@@ -0,0 +1,13 @@
+n,m = map(int,input().split(' '))
+num = list(map(int,input().split(" ")))
+num.sort()
+result = m
+for i in range(len(num)-2):
+    for j in range(1,len(num)-1):
+        for q in range(2,len(num)):
+            sum_num = num[i]+num[j]+num[q]
+            if(sum_num>m):
+                continue
+            if( m-sum_num<result):
+                result = m-sum_num
+print(m-result)
