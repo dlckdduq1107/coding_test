2634b20a0bcc71aab0d503bfdded518f2bc5a819
이창엽(Fansor) <dlckdduq97@gmail.com>
2022-05-31 01:35:10 +0900

    2022.5.31

---
+++ Solutions/하루 한문제 코테준비/주차요금계산.py
@@ -0,0 +1,47 @@
+from collections import defaultdict
+import math
+
+def sub(start,end):
+    atime,amin = start.split(":")
+    btime,bmin = end.split(":")
+    st,et = int(atime)*60+int(amin),int(btime)*60+int(bmin)
+    return et-st
+def cal(time, basic_time, basic_cost, per_time, per_cost):
+    if(time<=basic_time):
+        return basic_cost
+    else:
+        return basic_cost + math.ceil((time-basic_time)/per_time)*per_cost
+    
+def solution(fees, records):
+    answer = []
+    start = defaultdict(str)
+    end = defaultdict(str)
+    result = defaultdict(int)
+    total = defaultdict(int)
+    basic_time, basic_cost, per_time, per_cost = fees
+    for i in records:
+        time, car_num, state = i.split(" ")
+        if(state == "IN"):
+            start[car_num] = time
+            end[car_num] = "NOT"
+        else:
+            # result[car_num] += cal(sub(start[car_num],time), basic_time, basic_cost, per_time, per_cost)
+            end[car_num] = time
+            total[car_num] += sub(start[car_num],time)
+            # print("a",result,"\n", start,"\n",end)
+    # print(start,end)
+    for num in sorted(start.keys()):
+        start_time = start[num]
+        if(end[num] == "NOT"):
+            end_time = '23:59'
+            total[num] += sub(start[num],end_time)
+    print(total)
+        # else:
+        #     end_time = end[num]
+            # result[car_num] += cal(sub(start_time,end_time), basic_time, basic_cost, per_time, per_cost)
+    for i in sorted(total.keys()):
+        t = cal(total[i], basic_time, basic_cost, per_time, per_cost)
+        answer.append(t)
+    print(result)
+    return answer
+print(solution([180, 5000, 10, 600], ["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"]))
