d4a90ffb53946d6afc0650d4b75de9d87d0df3f6
이창엽(Fansor) <dlckdduq97@gmail.com>
2022-06-02 01:35:10 +0900

    2022.6.2

---
+++ Solutions/하루 한문제 코테준비/양궁대회.py
@@ -0,0 +1,41 @@
+import copy
+
+
+def cal_num(lion,apeach):
+    lion_total,apeach_total = 0,0
+    for i in range(11):
+        l,a = lion[i], apeach[i]
+        if(l+a==0):
+            continue
+        if(l>a):
+            lion_total += 10-i
+        else:
+            apeach_total += 10-i
+    return lion_total-apeach_total
+
+def dfs(lion_list,count,idx,info,n):
+    global differ,answer
+    if(idx>10):
+        return
+    if(count==n):
+        temp = cal_num(lion_list, info)
+        if(temp>0):
+            if(differ<temp):
+                answer = copy.deepcopy(lion_list)
+                differ = temp
+    else:
+        lion_list[idx] += 1
+        dfs(lion_list,count+1,idx,info,n)
+        lion_list[idx] -= 1
+
+        dfs(lion_list,count,idx+1,info,n)
+def solution(n, info):
+    global differ,answer
+    arr = [0 for i in range(11)]
+    differ = int(-1e9)
+    dfs(arr,0,0,info,n)
+    if(differ==int(-1e9)):
+        answer = [-1]
+    return answer
+
+print(solution(5,[2,1,1,1,0,0,0,0,0,0,0]))
