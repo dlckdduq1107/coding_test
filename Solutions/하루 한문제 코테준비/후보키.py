b3075c4fd039018a924e3046edf514546a9f0f7b
이창엽(Fansor) <dlckdduq97@gmail.com>
2022-06-19 01:35:10 +0900

    문제 진행중중

---
+++ Solutions/하루 한문제 코테준비/후보키.py
@@ -0,0 +1,10 @@
+from itertools import combinations
+def solution(relation):
+    answer = 0
+    total = len(relation)
+    res = [] 
+    for i in range(1,5):
+        for j in combinations(range(4),i):
+            print(j)
+    return answer
+print(solution([["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","4"],["500","muzi","music","3"],["600","apeach","music","2"]]))
