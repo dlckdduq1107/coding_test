8b87675b2338d6694cfa69eeab7d026491ae7955
이창엽(Fansor) <dlckdduq97@gmail.com>
2022-07-03 01:35:10 +0900

    2022.7.3
    정확성만 맞는 코드

---
+++ Solutions/하루 한문제 코테준비/가사검색.py
@@ -0,0 +1,20 @@
+import re
+def solution(words, queries):
+    answer = []
+    for each in queries:
+        q_num = list(each).count("?")
+        q_re = re.compile('[?]+')
+        sub = q_re.findall(each)[0]
+        re_each = each.replace(sub,'.{%s}' %q_num)
+        q = re.compile(re_each)
+
+        total=0
+        for w in words:
+            res = q.findall(w)
+            if(len(res)!=0 and len(res[0])==len(w)):
+                total += 1
+            print(res, re_each)
+        answer.append(total)
+    return answer
+
+print(solution(["frodo", "front", "frost", "frozen", "frame", "kakao"], ["fro??", "????o", "fr???", "fro???", "pro?"]))
