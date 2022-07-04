afe786b593d662520494d8382eb9485596186b73
이창엽(Fansor) <dlckdduq97@gmail.com>
2022-07-05 00:10:57 +0900

    2022.6.7
    이전 문제 설명 추가

--- Solutions/하루 한문제 코테준비/양과늑대.py
+++ Solutions/하루 한문제 코테준비/양과늑대.py
@@ -1,6 +1,6 @@
 def solution(info, edges):
     answer = []
-    visited = [0 for i in range(len(info))]
+    visited = [0 for i in range(len(info))]# 어디에 방문 했는지 체크
     visited[0] = 1
     def dfs(sheep, wolf):
         if(sheep > wolf):
@@ -9,7 +9,7 @@ def solution(info, edges):
             return
         for parent, child in edges:
             is_wolf = info[child]
-            if(visited[parent] and not visited[child]):
+            if(visited[parent] and not visited[child]): # 부모노드를 방문하고, 자식노드를 방문하지 않아야 양이든 늑대든 더해줄 수 있다. 엣지들을 dfs마다 전부 돌면서 다 체크해야 모든 경우를 다 탐색할 수 있다.
                 visited[child] = 1
                 dfs(sheep+abs(is_wolf-1), wolf + is_wolf)
                 visited[child] = 0
