b4c4c981e269f2933abca81415a726d81e5fd1c6
이창엽(Fansor) <dlckdduq97@gmail.com>
2022-07-08 01:35:10 +0900

    2022.7.8

--- Solutions/하루 한문제 코테준비/기둥과보설치.py
+++ Solutions/하루 한문제 코테준비/기둥과보설치.py
@@ -1,50 +1,29 @@
-def check(gidung,x,y,graph,n):
-    if(gidung==0):
-        if(x==0 or (y-1>=0 and graph[x][y-1][1]==1) or (x-1>=0 and graph[x-1][y][0]==1)):
-            return True
-        return False
-    else:
-        if((x-1>=0 and graph[x-1][y][0]==1) or (y+1<=n and x-1>=0 and graph[x-1][y+1][0]==1) or ((y-1>=0 and graph[x][y-1][1]==1) and (y+1<=n and graph[x][y+1][1]==1))):
-            return True
-        return False
+def check(result):
+    for x,y,t in result:
+        if(t==0):
+            if(y==0 or ([x-1,y,1] in result) or ([x,y,1] in result) or ([x,y-1,0] in result)):
+                continue
+            return False
+        else:
+            if(([x,y-1,0] in result) or ([x+1,y-1,0] in result) or (([x-1,y,1] in result) and ([x+1,y,1] in result))):
+                continue
+            return False
+    return True
 
 def solution(n, build_frame):
     answer = []
-    graph = [[[0,0] for i in range(n+1)] for j in range(n+1)] # 기둥,보
-    # for i in graph:
-    #     print(*i)
-    for y,x,t,build in build_frame:
-        if(build): # 설치
-            if(t==0):#기둥
-                if(x==0 or (y-1>=0 and graph[x][y-1][1]==1) or (x-1>=0 and graph[x-1][y][0]==1)):
-                    graph[x][y][0] = 1
-            else:
-                if((x-1>=0 and graph[x-1][y][0]==1) or (y+1<=n and x-1>=0 and graph[x-1][y+1][0]==1) or ((y-1>=0 and graph[x][y-1][1]==1) and (y+1<=n and graph[x][y+1][1]==1))):
-                    graph[x][y][1] = 1
-        else: # 삭제
-            if(t==0):
-                graph[x][y][0] = 0
-                if(check(1,x+1,y-1,graph,n) and check(0,x+1,y,graph,n) and check(1,x+1,y,graph,n)):
-                    pass
-                else:
-                    graph[x][y][0] = 1
-            else:
-                graph[x][y][1] = 0
-                if(check(1,x,y-1,graph,n) and check(1,x,y+1,graph,n) and check(0,x,y+1,graph,n)):
-                    pass
-                else:
-                    graph[x][y][1] = 1
-        # for i in range(n,-1,-1):
-        #     print(*graph[i])
-        # print('\n\n\n\n')
-    # for i in range(n,-1,-1):
-    #     print(*graph[i])
-    for idx,i in enumerate(graph):
-        for idxj, j in enumerate(i):
-            if(sum(j)==1):
-                answer.append([idxj,idx,0 if j[0]==1 else 1])
+    for x,y,t,build in build_frame:
+        if(build):
+            answer.append([x,y,t])
+            if(not check(answer)):
+                answer.remove([x,y,t])
+        else:
+            answer.remove([x,y,t])
+            if(not check(answer)):
+                answer.append([x,y,t])
     answer.sort(key=lambda x:x[2])
     answer.sort(key=lambda x:x[1])
     answer.sort(key=lambda x:x[0])
+
     return answer
 print(solution(5,[[0,0,0,1],[2,0,0,1],[4,0,0,1],[0,1,1,1],[1,1,1,1],[2,1,1,1],[3,1,1,1],[2,0,0,0],[1,1,1,0],[2,2,0,1]]))
