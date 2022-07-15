985aa43a5499e85eab06b6c758851565c08ae257
이창엽(Fansor) <dlckdduq97@gmail.com>
2022-07-16 01:35:10 +0900

    2022.7.13

---
+++ Solutions/하루 한문제 코테준비/텀프로젝트.py
@@ -0,0 +1,22 @@
+def dfs(start):
+    visited[start] = True
+    num = graph[start]
+    cycle.append(start)
+    if(visited[num]):
+        if(num in cycle):
+            result.extend(cycle[cycle.index(num):])
+    else:
+        dfs(num)
+n = int(input())
+for i in range(n):
+    people = int(input())
+    graph = [0]
+    graph.extend(list(map(int,input().split(' '))))
+    visited = [True]+[False for j in range(people)]
+    result = []
+    for j in range(1,people+1):
+        if(not visited[j]):
+            cycle = []
+            dfs(j)
+    # print(result)
+    print(people-len(result))
