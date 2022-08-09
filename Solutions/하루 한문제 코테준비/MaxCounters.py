01adf03fc2769fc228bdeafe1683992156628fae
이창엽(Fansor) <dlckdduq97@gmail.com>
2022-08-09 23:15:38 +0900

    2022.8.9

--- Solutions/하루 한문제 코테준비/MaxCounters.py
+++ Solutions/하루 한문제 코테준비/MaxCounters.py
@@ -1,9 +1,16 @@
 def solution(N, A):
     counter = [0 for i in range(N)]
-    # max_value = -int(1e9)
+    max_value = 0
+    save = 0
     for i in A:
         if(N+1==i):
-            counter = [max(counter) for i in range(N)]
+            save = max_value
         else:
+            if(counter[i-1]<save):
+                counter[i-1] = save
             counter[i-1] += 1
+            max_value = max(counter[i-1],max_value)
+    for idx,i in enumerate(counter):
+        if(i<save):
+            counter[idx] = save
     return counter
