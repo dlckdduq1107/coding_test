587ac1a4b3becb3ae8689142bcd7299adfaf862d
이창엽(Fansor) <dlckdduq97@gmail.com>
2022-08-19 14:23:35 +0900

    유니온 파인드로 풀었는데 예외처리를 덜한 듯

--- Solutions/하루 한문제 코테준비/거짓말.py
+++ Solutions/하루 한문제 코테준비/거짓말.py
@@ -1,31 +1,46 @@
+
+def union(a,b):
+    root_a = find(a)
+    root_b = find(b)
+    if(root_a>root_b):
+        parents[root_a] = root_b
+    else:
+        parents[root_b] = root_a
+        
+def find(x):
+    if(parents[x]!=x):
+        parents[x] = find(parents[x])
+    return parents[x]
+
 people, party = map(int, input().split(" "))
+parents = [i for i in range(people+1)]
 truth = list(map(int,input().split(' ')))
 if(len(truth)>1):
     truth = truth[1:]
 
+for i in truth:
+    parents[i] = 0
+
 total_party = []
 for i in range(party):
-    each_party = list(map(int,input().split(" ")))[1:]
+    each_party = list(map(int,input().split(" ")))[1:]#
     total_party.append(each_party)
-    for j in each_party:
-        if(j in truth):
-            truth.extend(each_party)
-            break
+    for j in range(len(each_party)-1):
+        union(each_party[j],each_party[j+1])
 
-for i in range(party-1,-1,-1):
-    for j in total_party[i]:
-        if(j in truth):
-            truth.extend(total_party[i])
-            break
-
-truth = set(truth)
+# print(parents)
+# print(total_party)
 result = 0
 for each in total_party:
-    result += 1
-    for j in each:
-        if(j in truth):
-            result -=1
+    print(each)
+    flag = True
+    for i in each:
+        if(find(i)==0):
+            flag = False
             break
+    if(flag):
+        result += 1
+            # print(each)
 print(result)
 # 예외처리 잘해야 할듯(에지 케이스)
 # 유니온 파인드로 풀어도 되고 집합으로 풀어도 된다.
