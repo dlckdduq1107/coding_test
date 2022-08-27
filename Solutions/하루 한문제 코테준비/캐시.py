416cb48ccdd78d960ff19630d61e3fb855357017
이창엽(Fansor) <dlckdduq97@gmail.com>
2022-08-27 09:37:58 +0900

    2022.8.27

---
+++ Solutions/하루 한문제 코테준비/캐시.py
@@ -0,0 +1,21 @@
+def solution(cacheSize, cities):
+    answer = 0
+    cache = []
+    for i in cities:
+        if(cacheSize==0):
+            answer += 5
+            continue
+        if(i in cache):#hit
+            cache.remove(i)
+            cache.append(i)
+            answer += 1
+        else:#miss
+            # if(cache != []):
+            #     cache.pop(0)
+            if(len(cache)==cacheSize):
+                cache.pop(0)
+            cache.append(i)
+            answer += 5
+        # print(cache)
+    return answer
+print(solution(0,["Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
