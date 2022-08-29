f4f8c912bc3be5b58e2e1d1081807778d64f81ce
이창엽(Fansor) <dlckdduq97@gmail.com>
2022-08-29 10:15:38 +0900

    쉽게 접근해서 풀자(압축)

--- Solutions/하루 한문제 코테준비/압축.py
+++ Solutions/하루 한문제 코테준비/압축.py
@@ -6,23 +6,17 @@ def solution(msg):
     for i in range(1,27):
         dictionary[chr(64+i)] = i
     
-    i = 0
-    while(i<len(msg)):#글자 반복
-        for j in range(pivot,0,-1):#글자수 맞을때까지 반복
-            if(i+j>len(msg)):
-                continue
-            each = msg[i:i+j]
-            print(each,j)
-            if(each in dictionary):
-                answer.append(dictionary[each])
-                if(i+j<len(msg)):
-                    dictionary[each+msg[i+j]] = last
-                last +=1
-                i += j
-                pivot = j+1
-                break
-        
+    w,c = 0,0
+    while True:
+        c+=1
+        if(c==len(msg)):
+            answer.append(dictionary[msg[w:c+1]])
+            break
+        if(msg[w:c+1] not in dictionary):
+            dictionary[msg[w:c+1]] = len(dictionary)+1
+            answer.append(dictionary[msg[w:c]])
+            w = c
 
-    print(dictionary)
+    # print(dictionary)
     return answer
 print(solution("TOBEORNOTTOBEORTOBEORNOT"))
