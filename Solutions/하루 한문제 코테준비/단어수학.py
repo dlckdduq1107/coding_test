2e54533088cd5a9d4ac3f398cc61e01e9d1d771a
이창엽(Fansor) <dlckdduq97@gmail.com>
2022-09-08 20:51:20 +0900

    단어 수학 설명
    큰자리 숫자에 위치하는대로 높은 숫자를 배정해야 하므로 9~0으로 배정하지 말고 10의 제급승으로 스코어를 매기자

--- Solutions/하루 한문제 코테준비/단어수학.py
+++ Solutions/하루 한문제 코테준비/단어수학.py
@@ -11,16 +11,14 @@ for alphas in each_alpha:
     length = len(alphas)
     for i in range(length, 0,-1):
         score[alphas[length-i]] += 10**i
-# print(score)
+
 temp = []
 for i in score:
     temp.append([i,score[i]])
 temp.sort(key=lambda x:x[1], reverse=True)
-# print(temp)
 res = {}
 for idx,each in enumerate(temp):
     res[each[0]] = 9-idx
-# print(res)
 answer = 0
 for i in each_alpha:
     temp = ''
