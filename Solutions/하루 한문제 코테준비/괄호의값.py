9442855efa7d21d36f40019e9c7aeb3dd600f7c9
이창엽(Fansor) <dlckdduq97@gmail.com>
2022-10-11 21:15:00 +0900

    2022.10.11

--- Solutions/하루 한문제 코테준비/괄호의값.py
+++ Solutions/하루 한문제 코테준비/괄호의값.py
@@ -2,33 +2,33 @@ gal = input()
 result = 0
 temp = 1
 stack = []
-for i in gal:
+for idx,i in enumerate(gal):
     if(i =='('):
         temp *=2
         stack.append(i)
     elif(i==')'):
-        if(not stack and stack[-1]=='['):
+        if(not stack or stack[-1]=='['):
             result = 0
             break
             
-        if(stack[-1]=='('):
+        if(gal[idx-1]=='('):
             result += temp
-            temp //=2
-            stack.pop()
+        temp //=2
+        stack.pop()
 
     elif(i=='['):
         temp *=3
         stack.append(i)
     elif(i==']'):
-        if(not stack and stack[-1]=='('):
+        if(not stack or stack[-1]=='('):
             result = 0
             break
             
-        else:
+        if(gal[idx-1]=='['):
             result += temp
-            temp//=3
-            stack.pop()
-    print(stack,result)
+        temp//=3
+        stack.pop()
+    # print(stack,result)
 if( stack):
     print(0)
 else:
