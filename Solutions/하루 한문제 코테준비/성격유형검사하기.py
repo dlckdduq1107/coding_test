80ba1e9d43c54f90548c91f5dbd4ed83ed235b12
이창엽(Fansor) <dlckdduq97@gmail.com>
2022-10-20 22:12:00 +0900

    2022.10.20

---
+++ Solutions/하루 한문제 코테준비/성격유형검사하기.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+def solution(survey, choices):
+    answer = ''
+    personality = defaultdict(int)
+    each_personal = [['R','T'],['C','F'],['J','M'],['A','N']]
+
+    for i in range(len(survey)):
+        [disagree, agree] = list(survey[i])
+        num = choices[i]
+
+        if(4-num > 0):
+            personality[disagree] += abs(4-num)
+        else:
+            personality[agree] += abs(4-num)
+        # print(personality,num)
+    for first,second in each_personal:
+        if(personality[first]<personality[second]):
+            answer += second
+        else:
+            answer += first
+    return answer
+
+print(solution(["AN", "CF", "MJ", "RT", "NA"], [5, 3, 2, 7, 5]))
