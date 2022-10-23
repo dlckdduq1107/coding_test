d20f4db0f638d0b582fe63a100bb5273daff18a5
이창엽(Fansor) <dlckdduq97@gmail.com>
2022-10-23 22:11:12 +0900

    2022.10.23

--- Solutions/하루 한문제 코테준비/두큐합같게만들기.py
+++ Solutions/하루 한문제 코테준비/두큐합같게만들기.py
@@ -1,17 +1,37 @@
+import copy
+from collections import deque
 def solution(queue1, queue2):
-    answer = 0
-    while(sum(queue1)!=sum(queue2)):
-        if(sum(queue1)>sum(queue2)):
-            pop_each = queue1.pop(0)
-            queue2.append(pop_each)
-        else:
-            pop_each = queue2.pop(0)
-            queue1.append(pop_each)
-        answer += 1
-        if(answer > len(queue1)*2):
-            answer = -1
-            break
+    answer = 1
+    first,second = deque(copy.deepcopy(queue1)), deque(copy.deepcopy(queue2))
 
+    if(sum(first) == sum(second)):
+        return 0
+    
+    else:
+        if(sum(first)>sum(second)):
+            popper = first.popleft()
+            second.append(popper)
+        else:
+            popper = second.popleft()
+            first.append(popper)
+    flag = True
+    for i in range(len(queue1)*4):
+        if(sum(first)>sum(second)):
+            popper = first.popleft()
+            second.append(popper)
+        elif(sum(first)==sum(second)):
+            flag = False
+            break
+        else:
+            popper = second.popleft()
+            first.append(popper)
+        answer += 1
+        
+    
+    if(flag):
+        return -1
     return answer
 
 print(solution([3, 2, 7, 2],[4, 6, 5, 1]))
+print(solution([1, 2, 1, 2]	,[1, 10, 1, 2]	))
+print(solution([1, 1]	,[1, 5]	))
