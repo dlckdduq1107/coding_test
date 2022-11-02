d16efc0340480ad3a6fc3c27b006300e6f42ede9
이창엽(Fansor) <dlckdduq97@gmail.com>
2022-11-02 23:09:43 +0900

    2022.11.2

--- Solutions/하루 한문제 코테준비/전구와스위치.py
+++ Solutions/하루 한문제 코테준비/전구와스위치.py
@@ -1,3 +1,6 @@
+
+
+
 from copy import copy, deepcopy
 
 
@@ -13,29 +16,35 @@ def change(index,arr):
         arr[index] = not arr[index]
         arr[index+1] = not arr[index+1]
     
-
+def switchFirst(isClick,current_state,target_state):
+    result = 0
+    for i in range(n):
+        if(i==0):
+            if(isClick):
+                change(i,current_state)
+                result += 1
+        else:
+            if(current_state[i-1]!=target_state[i-1]):
+                change(i,current_state)
+                result += 1
+        
+    if(current_state == target_state):
+        return result
+    else:
+        return -1
 n = int(input())
 current_state = list(map(int,list(input())))
 current_state = [False if i==0 else True for i in current_state]
 target_state = list(map(int,list(input())))
 target_state = [False if i==0 else True for i in target_state]
 
-cpy_current = deepcopy(current_state)
-idx = 0
-result = 0
-while(current_state != target_state ):
-    if(current_state[idx] != target_state[idx]):
-        change(idx,current_state)
-        result += 1
-    if(idx==n-1):
-        idx = 0
-    else:
-        idx += 1
-    if(result != 0 and current_state == cpy_current):
-        break
-    # print(current_state)
-    # print(target_state)
-if(current_state==target_state):
-    print(result)
-else:
+res1 = switchFirst(True, deepcopy(current_state), target_state)
+res2 = switchFirst(False, deepcopy(current_state), target_state)
+
+if(res1>=0 and res2>=0):
+    print(min(res1,res2))
+elif(res1==-1 and res2==-1):
     print(-1)
+elif(res1==-1 or res2==-1):
+    print(max(res1,res2))
+
