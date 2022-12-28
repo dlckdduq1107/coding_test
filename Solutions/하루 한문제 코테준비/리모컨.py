911d2a309d478028c6483d513699e9d95eb9793c
이창엽(Fansor) <dlckdduq97@gmail.com>
2022-12-28 20:16:06 +0900

    2022.12.28

---
+++ Solutions/하루 한문제 코테준비/리모컨.py
@@ -0,0 +1,17 @@
+n = int(input())
+error_key = int(input())
+if(error_key != 0):
+    error_list = list(map(int,input().split(" ")))
+    min_count = abs(100-n)
+
+    for i in range(1000001):
+        temp = str(i)
+        for jdx,j in enumerate(temp):
+            if(int(j) in error_list):
+                break
+            if(jdx == len(temp)-1):
+                min_count = min(min_count,abs(i-n)+len(temp))
+    print(min_count)
+            
+else:
+    print(len(str(n)))
