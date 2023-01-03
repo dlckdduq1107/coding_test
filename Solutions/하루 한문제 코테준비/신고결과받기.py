e4024d964a4be5dac067ac159a6cfcdce4b62402
이창엽(Fansor) <dlckdduq97@gmail.com>
2023-01-03 20:49:06 +0900

    2023.1.3

--- Solutions/하루 한문제 코테준비/신고결과받기.py
+++ Solutions/하루 한문제 코테준비/신고결과받기.py
@@ -1,20 +1,20 @@
+from collections import defaultdict
 def solution(id_list, report, k):
     answer = []
-    limit = {}
-    gobal = {}
+    attacked = defaultdict(set)
+    for each in report:
+        fromUser, toUser = each.split(' ')
+        attacked[toUser].add(fromUser)
+    # print(attacked)
+    fromUserSet = defaultdict(int)
+    for i in attacked.keys():
+        eachAttacked = attacked[i]
+        if(len(eachAttacked)>=k):
+            for j in eachAttacked:
+                fromUserSet[j] += 1
+    # print(fromUserSet)
     for i in id_list:
-        limit[i] = 0
-        gobal[i] = []
-    for i in report:
-        attack, target = i.split(" ")
-        if(target not in gobal[attack]):
-            gobal[attack].append(target)
-            limit[target] += 1
-    
-    for i in id_list:
-        temp = 0
-        for j in gobal[i]:
-            if(limit[j]>=k):
-                temp += 1
-        answer.append(temp)
+        answer.append(fromUserSet[i])
     return answer
+
+print(solution(["muzi", "frodo", "apeach", "neo"],["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"], 2))
