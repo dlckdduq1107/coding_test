d5dca0cee08006ea4808535d0bbf622ad7f3e916
이창엽(Fansor) <dlckdduq97@gmail.com>
2023-03-27 20:35:52 +0900

    23.3.26

--- Solutions/하루 한문제 코테준비/분수합.py
+++ Solutions/하루 한문제 코테준비/분수합.py
@@ -1,14 +1,11 @@
-def checkPrime(num):
-    divides = set()
-    if(num<3):
-        return [False, set([1])]
-    for i in range(2, num-1):
-        if(num%i == 0):
-            divides.add(i)
-    if(len(divides)==0):
-        return [False,set([1])]
-    else:
-        return [True,divides]
+
+def gcd(a,b):
+    while(a%b!=0):
+        mod = a%b
+        a = b
+        b = mod
+    return b
+        
 fchild, dparent = map(int, input().split(" "))
 
 schild, sparent = map(int, input().split(" "))
@@ -16,15 +13,5 @@ schild, sparent = map(int, input().split(" "))
 parent = dparent*sparent
 child = fchild*sparent + dparent*schild
 
-fcheck,fset = checkPrime(parent)
-scheck, sset = checkPrime(child)
-
-# print(fset, sset)
-res_set = fset&sset
-target = 1
-# print(res_set)
-if(len(res_set) != 0):
-    for i in list(res_set):
-        target *= i
-
-print(child//target, parent//target)
+gcd_num = gcd(parent, child)
+print(child//gcd_num, parent//gcd_num)
